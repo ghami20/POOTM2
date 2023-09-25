9513619b9626ff37c79f07dd42986030c2b8f942
escuela@davinci.edu.ar <ghamiboxbox@gmail.com>
2023-09-25 11:44:51 -0300

    Inicio de programa

---
+++ src/Paquete/Main.java
@@ -0,0 +1,12 @@
+package Paquete;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hola mundo");
+
+	}
+
+}
